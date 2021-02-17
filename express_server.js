a91a94ef3a55a0b7d52d3fb2ffc3d05ad018f1cf
Sandra To <sandra.to.e@gmail.com>
2021-02-17 05:02:51 +0000

    Added templateVars with username key-value in new url route

--- express_server.js
+++ express_server.js
@@ -52,7 +52,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
