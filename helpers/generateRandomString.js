460668cbeb0ce958af0722f65178c7c64327c433
Sandra To <sandra.to.e@gmail.com>
2021-02-18 07:11:08 +0000

    Added recursive case to func to ensure 6 digit str returned

--- helpers/generateRandomString.js
+++ helpers/generateRandomString.js
@@ -1,6 +1,11 @@
 const generateRandomString = () => {
   let str = Math.random().toString(36).substring(7);
+  if (str.length !== 6) {
+    str = generateRandomString();
+  }
   return str;
 };
+
+console.log(generateRandomString());
 
 module.exports = { generateRandomString };
