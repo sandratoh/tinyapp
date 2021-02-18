87d1c15f03f27b48d8171e43b8fe0c7623e4042d
Sandra To <sandra.to.e@gmail.com>
2021-02-18 05:07:49 +0000

    Upated function so only url owner can update urls

--- helpers/urlHelpers.js
+++ helpers/urlHelpers.js
@@ -2,8 +2,11 @@ const deleteURL = (urlObj, key) => {
   delete urlObj[key];
 };
 
-const updateURL = (urlObj, key, newURL) => {
-  return urlObj[key] = newURL;
+const updateURL = (urlObj, key, newURL, user) => {
+  return urlObj[key] = {
+    longURL: newURL,
+    userID: user
+  };
 };
 
 module.exports = { deleteURL, updateURL };
