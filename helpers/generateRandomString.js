57eebfb585c5da4952877066d8aee890047aa2fe
Sandra To <sandra.to.e@gmail.com>
2021-02-19 07:06:38 +0000

    Renamed or moved files

--- helpers/generateRandomString.js
+++
@@ -1,9 +0,0 @@
-const generateRandomString = () => {
-  let str = Math.random().toString(36).substring(7);
-  if (str.length !== 6) {
-    str = generateRandomString();
-  }
-  return str;
-};
-
-module.exports = { generateRandomString };
